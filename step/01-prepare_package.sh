90fd407a50ee73ad5a06515917996a59582d6467
Gyj1109 <gaoyj1109@gmail.com>
2020-07-14 16:21:45 +0800

    移除无用补丁

--- step/01-prepare_package.sh
+++ step/01-prepare_package.sh
@@ -180,7 +180,6 @@ git clone https://github.com/rufengsuixing/luci-app-zerotier package/lean/luci-a
 svn co https://github.com/coolsnowwolf/packages/trunk/net/zerotier package/lean/zerotier
 #OLED display
 git clone https://github.com/natelol/luci-app-oled package/natelol/luci-app-oled
-wget -O package/natelol/luci-app-oled/root/etc/init.d/oled https://github.com/msylgj/luci-app-oled/raw/patch-1/root/etc/init.d/oled
 #Syncthing
 find package -name "*syncthing*" | xargs rm -rf
 git clone https://github.com/songchenwen/openwrt-package.git songchenwen
