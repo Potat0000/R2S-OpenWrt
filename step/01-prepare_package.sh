a4b3d8d3014ff3ea584ea1ae7a4823f6182ca2db
Gyj1109 <gaoyj1109@gmail.com>
2020-07-14 08:25:24 +0800

    Revert "修改autocore补丁方式"
    This reverts commit 0bba07beb6f275c7842bfdce4613bcaccc55ef9a.

--- step/01-prepare_package.sh
+++ step/01-prepare_package.sh
@@ -58,7 +58,7 @@ svn co https://github.com/coolsnowwolf/lede/trunk/package/lean/luci-app-arpbind 
 #AutoCore
 #svn co https://github.com/project-openwrt/openwrt/branches/openwrt-19.07/package/lean/autocore package/lean/autocore
 svn co https://github.com/project-openwrt/openwrt/branches/master/package/lean/autocore package/lean/autocore
-git apply ../patches/enable_autocore.diff
+sed -i "s,@TARGET_x86 ,,g" package/lean/autocore/Makefile
 rm -rf ./package/lean/autocore/files/cpuinfo
 wget -P package/lean/autocore/files https://raw.githubusercontent.com/QiuSimons/Others/master/cpuinfo
 rm -rf ./package/lean/autocore/files/rpcd_10_system.js
