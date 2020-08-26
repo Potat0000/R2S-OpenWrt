40dc5210b4df93a8a18780794dedaa5fab69acbb
Gyj1109 <gaoyj1109@gmail.com>
2020-08-26 20:06:09 +0800

    修复错误

--- scripts/cisteps/build-openwrt/14-organize_files.sh
+++ scripts/cisteps/build-openwrt/14-organize_files.sh
@@ -5,19 +5,20 @@ shopt -s extglob
 
 sudo chown -R "$(id -u):$(id -g)" "${HOST_BIN_DIR}"
 
-mv "${HOST_BIN_DIR}/.config" "${HOST_BIN_DIR}/config.seed"
+mv "${HOST_BIN_DIR}/.config" "${HOST_BIN_DIR}/config.seed" || true
 
-if [ "x${OPT_PACKAGE_ONLY}" != "x1" ]; then
+if [ "x${OPT_PACKAGE_ONLY}" != "x1" ] && [ "x${TEST}" != "x1" ]; then
   mkdir "${HOST_WORK_DIR}/openwrt_firmware"
   cd "${HOST_BIN_DIR}/targets/"*/*
   all_firmware_files=( !(packages) )
   [ ${#all_firmware_files[@]} -gt 0 ] && mv "${all_firmware_files[@]}" "${HOST_WORK_DIR}/openwrt_firmware/" || true
-  cp "${HOST_BIN_DIR}/config.seed" "${HOST_WORK_DIR}/openwrt_firmware/"
+
+  cp "${HOST_BIN_DIR}/config.seed" "${HOST_WORK_DIR}/openwrt_firmware/" || true
 
   cd "${HOST_WORK_DIR}"
   mkdir -p "${HOST_WORK_DIR}/firmware"
-  mv "openwrt_firmware/openwrt-*-squashfs-sysupgrade.img.gz" firmware/
-  mv "openwrt_firmware/config.seed" firmware/
+  mv openwrt_firmware/openwrt-*-squashfs-sysupgrade.img.gz firmware/
+  mv openwrt_firmware/config.seed firmware/
 
   cd firmware
   gzip -d *.gz || true
@@ -30,10 +31,10 @@ if [ "x${OPT_PACKAGE_ONLY}" != "x1" ]; then
   cp checksums "${HOST_WORK_DIR}/body.md"
   sed -i 's/:\ \+/: /g' "${HOST_WORK_DIR}/body.md"
   sed -i '1iBuilt at BUILDTIME' "${HOST_WORK_DIR}/body.md"
-  zip -r "${HOST_WORK_DIR}/BUILDNAME-${BUILD_TARGET}-BUILDTIME-Firmware.zip" *
+  zip -r "${HOST_WORK_DIR}/BUILDUSER-${BUILD_TARGET}-BUILDTIME-Firmware.zip" *
 fi
 
 cd "${HOST_BIN_DIR}"
-zip -r "${HOST_WORK_DIR}/BUILDNAME-${BUILD_TARGET}-BUILDTIME-Package" *
+zip -r "${HOST_WORK_DIR}/BUILDUSER-${BUILD_TARGET}-BUILDTIME-Package" *
 
 echo "::set-output name=status::success"
